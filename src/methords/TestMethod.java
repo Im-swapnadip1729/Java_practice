74c271e73d9b63c15aa78fbeb98b6fc5849d1ffd
swapnadip98 <123538663+swapnadip98@users.noreply.github.com>
2023-12-27 15:16:32 +0530

    Method first

---
+++ src/methords/TestMethod.java
@@ -0,0 +1,13 @@
+package methords;
+
+public class TestMethod {
+
+    public static void main(String[] args) {
+        ram();
+    }
+
+    //print jay sri ram method
+    public static void ram(){
+        System.out.println("Jay sri Ram ");
+    }
+}
